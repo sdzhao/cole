d8b48434d9bef0cd73dba2ae928607d92bd3eb07
Pickyilkis <hughywang0710@gmail.com>
2019-01-29 20:25:53 -0600

    fast push

--- R/cole.R
+++ R/cole.R
@@ -1,5 +1,7 @@
 #' @useDynLib cole
 #' @importFrom Rcpp evalCpp
+#' @importFrom camel camel.cmr
+#' @importFrom glmnet glmnet cv.glmnet
 #' @exportPattern "^[[:alpha:]]+"
 #' @import Rcpp RcppArmadillo
 NULL
