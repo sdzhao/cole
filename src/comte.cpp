f22c6bdefc2e580c04cb8c2b8858660d82e95748
pickyilkis <40706160+pickyilkis@users.noreply.github.com>
2019-01-31 18:49:27 -0600

    Yihe dev (#3)
    * update center and scale options
    
    * fast push
    
    * init
    
    * clean up
    
    * update comte

--- src/comte.cpp
+++ src/comte.cpp
@@ -29,7 +29,6 @@ using namespace Rcpp;
 //' @param tol error tolerance for convergence of EM algorithm. Default value of tol is 1e-6.
 //' @param maxit maximum number of allowable iterations. Default value of maxit is 1e5.
 //' @param min_s2 a positive number corresponds to minimal variance of estimated y, min_s2 is required when there are p columns in S.
-//'
 //' @return
 //' \item{f}{vector with \eqn{g x q} elements that describes the mixture of \eqn{\beta}s.}
 //' \item{A}{matrix with dimension \eqn{q x d}. A is an estimation of likelihood by EM algorithm and will be used in predicting.}
@@ -57,75 +56,51 @@ using namespace Rcpp;
 //' @useDynLib cole
 //' @export
 // [[Rcpp::export]]
-List comte(arma::mat y, arma::mat x, arma::mat S, double tol = 1e-6 , 
-	int maxit = 1e+5, Nullable<NumericVector> min_s2 = R_NilValue, 
-	float cutoff = 1e-250, bool center = false, bool scale = false){
-
-  if(center){
-
-  }
-  if(scale){
+List comte(arma::mat y, arma::mat x, arma::mat S, double tol = 1e-6, int maxit = 1e5, Nullable<NumericVector> min_s2 = R_NilValue){
   
-  }
-
   //initialization:
-  int mp1 = S.n_rows; // g * q
-  arma::vec f = arma::ones(mp1) / mp1;
   int p = x.n_cols;
   int q = y.n_cols;
+  int ps = S.n_cols;
+  int mp1 = S.n_rows; // g * q
+  arma::vec f = arma::ones(mp1) / mp1;
 
-  arma::mat B(mp1, p);
-  arma::mat ss(mp1, 1);
-  int p1 = S.n_cols;
-  if(p1 == p){
-    if(min_s2.isNull()){
-      std::cout << "user defined minimum sigma squared is required" << std::endl;
-    }else{
-      B = S;
-      NumericVector min_s2d(min_s2);
-      ss.fill(min_s2d(0));
-    }
-  }else if(p1 == p + 1){
-    B = S.submat(0,0,mp1-1,p-1);
-    ss = S.submat(0,p,mp1-1,p);
-  }else{
-    std::cout << "wrong dimension" << std::endl;
-  }
+  if(ps == p + 1){
+    arma::mat ss = S.submat(0,p,mp1-1,p);
+    arma::mat B = S.submat(0,0,mp1-1,p-1);
 
-  //indices:
-  arma::mat bs( mp1, p+1);
-  bs = arma::join_rows(B,ss);
+    //indices:
 
-  //compute likelihood
-  arma::mat ym = arma::repmat(y,1,mp1);
-  arma::mat xbm = arma::kron(x * B.t(), arma::ones(1,q));
-  arma::mat A(q, mp1);
+    arma::mat bs( mp1, p+1);
+    bs = arma::join_rows(B,ss);
 
-  //EM algorithm
-  arma::mat ll;
-  arma::mat oldll;
-  arma::mat diff;
-  arma::mat thres;
+    // //compute likelihood
+    arma::mat ym = arma::repmat(y,1,mp1);
+    arma::mat xbm = arma::kron(x * B.t(), arma::ones(1,q));
+    arma::mat ssm = arma::kron(ss.t(),arma::ones(1,q));
+    //arma::mat Avec = exp( (-arma::sum(pow((ym - xbm),2), 0))/(2*ssm))/pow(ssm, n/2) + pow(0.1,300);
+    arma::mat Avec = exp( (-arma::sum(pow((ym - xbm),2), 0))/(2*ssm) ) + pow(0.1,300);
+    arma::mat A = arma::reshape(Avec, q, mp1);
 
-  arma::mat denomiator;
-  arma::mat weight;
+    //EM algorithm
+    arma::mat ll; 
+    arma::mat oldll;
+    arma::mat diff;
+    arma::mat thres;
 
-  double err = pow(10,10);
-  arma::vec oldf;
-  int tol_iter = 0;
+    arma::mat denomiator;
+    arma::mat weight;
 
-  if(p1 == p + 1){
-  	arma::mat ssm = arma::kron(ss.t(),arma::ones(1,q));
-  	arma::mat Avec = exp( (-arma::sum(pow((ym - xbm),2), 0))/(2*ssm) ) + cutoff;
-    A = arma::reshape(Avec, q, mp1);
-
-  	for(int i = 0; i < maxit; i++){
+    double err = pow(10,10);
+    arma::vec oldf;
+    int tol_iter = 0;
+    for(int i = 0; i < maxit; i++){
       tol_iter += 1;
       oldf = f;
       thres = 1/(A * oldf);
       for(int j = 0; j < q; j++){
-        if( thres(j,0) > cutoff){
-          thres(j,0) = cutoff;
+        if( thres(j,0) > pow(10,300)){
+          thres(j,0) = pow(10,300);
         }
       }
 
@@ -134,33 +109,64 @@ List comte(arma::mat y, arma::mat x, arma::mat S, double tol = 1e-6 ,
       ll = sum(log(A * f));
       oldll = sum(log(A * oldf));
       diff = oldll - ll;
-      err = std::abs(diff(0,0)) / std::abs((oldll)(0,0));
+      err = abs(diff(0,0)) / abs((oldll)(0,0));
       if(err <= tol){
         break;
       }
-  	}
-  }else{
-  	arma::mat Avec = arma::sum(2*ym%xbm - xbm%xbm,0)/(2*ss(0,0));
-  	float maxA = Avec.max();
-  	Avec = exp(Avec / maxA);
-  	A = arma::reshape(Avec, q, mp1);
-
-  	for(int i = 0; i < maxit; i++){
-     tol_iter += 1;
-      oldf = f;
-      thres = 1/(A * oldf);
-      f = A.t() * (thres) % oldf /q;
-      ll = sum(log(A * f));
-      oldll = sum(log(A * oldf));
-      diff = oldll - ll;
-      err = abs(diff(0,0)) / abs((oldll)(0,0));
-      if(err <= tol){
-      	break;
-      }
     }
+
+    return List::create(_["f"] = f, _["A"] = A, _["bs"] = bs);
+  }else if(ps == p){
+    if(min_s2.isNull()){
+      std::cout << "user defined minimum sigma squared is required" << std::endl;
+      return List::create(_["Error"] = 1);
+    }else{
+      arma::mat B = S;
+      NumericVector min_s2d(min_s2);
+      arma::mat ss(mp1, 1);
+      ss.fill(min_s2d(0));
+
+      arma::mat bs( mp1, p+1);
+      bs = arma::join_rows(B,ss);
+
+      //compute likelihood
+      arma::mat A1 = arma::repmat(arma::sum(pow(y, 2),0).t(), 1, mp1);
+      arma::mat A2 = -2 * (x*B.t()).t() * y ;
+      arma::mat A3 = arma::repmat(arma::sum(pow(x*B.t(), 2), 0), q, 1);
+      arma::mat A = exp( (A1 + A2.t() + A3) /(-2*ss(0,0))); 
+
+      //EM algorithm
+      arma::mat ll; 
+      arma::mat oldll;
+      arma::mat diff;
+      arma::mat thres;
+
+      arma::mat denomiator;
+      arma::mat weight;
+
+      double err = pow(10,10);
+      arma::vec oldf;
+      int tol_iter = 0;
+      for(int i = 0; i < maxit; i++){
+        tol_iter += 1;
+        oldf = f;
+        thres = 1/(A * oldf);
+        f = A.t() * (thres) % oldf /q;
+        ll = sum(log(A * f));
+        oldll = sum(log(A * oldf));
+        diff = oldll - ll;
+        err = abs(diff(0,0)) / abs((oldll)(0,0));
+        if(err <= tol){
+          break;
+        }
+      }
+      return List::create(_["f"] = f, _["A"] = A, _["bs"] = bs);
+    }
+  }else{
+    std::cout << "S has wrong dimension!" << std::endl;
+    return List::create(_["Error"] = 1);
   }
 
-  return List::create(_["f"] = f, _["A"] = A, _["bs"] = bs);
 }
 
 
