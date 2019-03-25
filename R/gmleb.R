4e78ac655b5590510f89f86932cc28ce0ada450b
James J Balamuta <balamut2@illinois.edu>
2019-03-25 17:41:21 -0500

    Format R code style w/ styler.

--- R/gmleb.R
+++ R/gmleb.R
@@ -1,7 +1,7 @@
 #' General maximum likelihood empirical Bayes estimator of Jiang and Zhang (2009)
 #'
 #' Estimates mean vector of a homoscedastic sequence of independent Gaussian observations using the nonparametric maximum likelihood procedure of Jiang and Zhang (2009)
-#' 
+#'
 #'
 #' @param x1 Gaussian sequence
 #' @param s1 standard deviation of Gaussian sequence
@@ -10,7 +10,7 @@
 #' @param M number of grid points
 #' @param tol error tolerance of convergence of log likelihood
 #' @param maxit maximum number of iterations
-#' 
+#'
 #'
 #' @return
 #' estimated values of means of primary Gaussian sequence
@@ -25,41 +25,40 @@
 #' mean((theta1 - x1)^2)
 #' ## loss of GMLEB
 #' mean((theta1 - gmleb(x1, 1)$theta_hat)^2)
-#'
 #' @useDynLib cole
 #' @export
 
 gmleb = function(x1, s1,
-                 init = NULL, grid = NULL, M = 300,
-                 tol = 1e-5, maxit = 1000) {
-    
-    ## error checking
-    if (length(s1) != 1) {
-        stop("s1 must be scalar")
-    }
-    
-    if(is.null(init[1])){
-        init = rep(1 / M, M)
-    }
-    if(is.null(grid[1])){
-        grid = seq(min(x1), max(x1), length = M)
-    }
-    
-    ## em algorithm
-    A = exp(-0.5 * (outer(x1, grid, "-")^2 / s1^2)) / s1
-    f = init
-    err = Inf
-    n = length(x1)
+                  init = NULL, grid = NULL, M = 300,
+                  tol = 1e-5, maxit = 1000) {
 
-    for(it in 1:maxit){
-        oldf = f
-        f = t(A) %*% (1 / (A %*% oldf)) * oldf / n
-        oldll = sum(log(A %*% oldf))
-        err = abs(sum(log(A %*% f)) - oldll) / abs(oldll)
-        if(err <= tol){
-            break
-        }
+  ## error checking
+  if (length(s1) != 1) {
+    stop("s1 must be scalar")
+  }
+
+  if (is.null(init[1])) {
+    init = rep(1 / M, M)
+  }
+  if (is.null(grid[1])) {
+    grid = seq(min(x1), max(x1), length = M)
+  }
+
+  ## em algorithm
+  A = exp(-0.5 * (outer(x1, grid, "-")^2 / s1^2)) / s1
+  f = init
+  err = Inf
+  n = length(x1)
+
+  for (it in 1:maxit) {
+    oldf = f
+    f = t(A) %*% (1 / (A %*% oldf)) * oldf / n
+    oldll = sum(log(A %*% oldf))
+    err = abs(sum(log(A %*% f)) - oldll) / abs(oldll)
+    if (err <= tol) {
+      break
     }
-    
-    return(list(theta_hat = drop(A %*% (f * grid) / A %*% f)))
+  }
+
+  return(list(theta_hat = drop(A %*% (f * grid) / A %*% f)))
 }
