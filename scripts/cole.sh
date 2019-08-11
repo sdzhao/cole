d26e361056c43cf010a115881512985f24b82d54
Pickyilkis <hughywang0710@gmail.com>
2019-08-11 13:36:26 -0400

    [DOCKER] build and run images

---
+++ scripts/cole.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# how to use the script:
+#
+# to build image, run:
+# ./scripts/cole.sh build
+#
+# to run rstudio in Docker, run:
+# ./scripts/cole.sh r
+
+ACTION=$1
+IMAGE="cole/base"
+
+if [[ -z $ACTION ]]; then
+    echo "Usage: cole.sh [build/r]"
+    exit 1
+fi
+
+if [[ $ACTION = 'build' ]];
+then
+    echo "Building Docker image: $IMAGE"
+    docker build . -t $IMAGE -f Dockerfiles/Dockerfile
+elif [[ $ACTION = 'r' ]];
+then
+    echo "Running R"
+    docker run --rm -p 8787:8787 -e PASSWORD=cole $IMAGE
+else
+    echo "Usage: cole.sh [build/r]"
+fi
